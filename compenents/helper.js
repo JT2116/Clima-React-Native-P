f427db390c0c8bc7d4cbb3284fcf5a3f98c82bf8
JT2116 <52418189+JT2116@users.noreply.github.com>
2023-04-03 17:00:23 -0400

    Delete compenents directory

--- compenents/helper.js
+++
@@ -1,5 +0,0 @@
-import AsyncStorage from "@react-native-async-storage/async-storage"
-
-export const storeAudioForNextOpening = (audio,index) => {
-    AsyncStorage.setItem('previousAudio',JSON.stringify({audio,index}))
-}
