b3ca6c4f0594e84d5ff235d80967679817ccaf27
JT2116 <52418189+JT2116@users.noreply.github.com>
2023-05-17 16:54:19 -0400

    Add files via upload

--- compenents/Citys.js
+++ compenents/Citys.js
@@ -1,53 +1,41 @@
 import React, { Component } from "react";
-import { StyleSheet } from 'react-native';
+import { StyleSheet,Dimensions } from 'react-native';
 import CitysListItem from "./CitysListItem";
 import Screen from "./Screen";
 import { RecyclerListView, LayoutProvider, DataProvider } from "recyclerlistview";
 
+const ViewTypes = {
+    FULL: 0,
+    HALF_LEFT: 1,
+    HALF_RIGHT: 2
+};
 
 export class Citys extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
                       
-        };        
+    //     };        
 
-        this.currentItem = {};
-    };
+    //     this.currentItem = {};
+    // };
+    
 
-    dataProvider = new DataProvider((r1, r2) => {
-        return r1 !== r2;
-    });
+    render() {       
+        // return(
+        //     <Screen>
+        //         {/* <CitysListItem/> */}    
+        //         <RecyclerListView
 
-    layoutProvider = new LayoutProvider(
-        (i) =>'audio',(type,dim)=>{
-            switch (type) {
-                case 'audio':
-                    dim.width = Dimensions.get('window').width;
-                    dim.height = 70;
-                    break;
-                default:
-                    dim.width = 0;
-                    dim.height = 0;
-            }
-
-        }
-    );
-
-    render() {      
-        // {({}) => {
-
-        // }} 
-        return(
-            <Screen>
-                {/* <CitysListItem/> */}    
-                <RecyclerListView
-                    layoutProvider={this.layoutProvider}
-                    dataProvider={this.dataProvider}
-                    rowRenderer={<CitysListItem/>}
+                    
                     
                 
-                />                    
+        //         />                    
+        //     </Screen>
+        // );
+        return(
+            <Screen>
+                <CitysListItem/>
             </Screen>
         );
     };
